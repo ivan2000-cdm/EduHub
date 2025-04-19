975c88f98dabef7623d7dc0aa235dc2cd0523a8d
ivan_git <ivan_niknorov2000@mail.ru>
2025-04-19 19:23:17 +0300

    Реализация CRUD-контроллера для справочника «Классы»

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-white shadow p-4 rounded">
     <h1 class="mb-3 text-center">Ура! Вы авторизовались!</h1>
-    <p v-if="token" class="text-center">Вот ваш токен:</p>
+    <p v-if="token" class="text-center">Вот ваш токен (для теста):</p>
     <div v-if="token" class="bg-light p-3 rounded" style="word-break: break-word; white-space: pre-wrap;">
       {{ token }}
     </div>
